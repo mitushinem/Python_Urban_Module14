31e69feb31b8386e0d9ebd5d0c5617a0ff874cb7
Evgeny M. Mitushin <mitushinem@gmail.com>
2024-09-07 16:35:24 +0300

    Домашнее задание по теме "Доработка бота"

---
+++ kb.py
@@ -0,0 +1,24 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
+
+kb = ReplyKeyboardMarkup(keyboard=[
+    [KeyboardButton('Купить')],
+    [KeyboardButton('Рассчитать'), KeyboardButton('Информация')]
+], resize_keyboard=True)
+
+kb_inline = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [InlineKeyboardButton('Рассчитать норму калорий', callback_data='calories')],
+        [InlineKeyboardButton('Формулы расчёта', callback_data='formulas')]
+    ]
+)
+
+kb_product_inline = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton('Product1', callback_data='product_buying|Product1'),
+            InlineKeyboardButton('Product2', callback_data='product_buying|Product2'),
+            InlineKeyboardButton('Product3', callback_data='product_buying|Product3'),
+            InlineKeyboardButton('Product4', callback_data='product_buying|Product4')
+        ]
+    ]
+)
